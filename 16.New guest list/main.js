20bab98426c7538832ffc86600628b45cb0f84d3
Syedakrizvi <kiranrizvi321@gmail.com>
2024-05-11 18:00:07 +0500

    1st change

--- 16.New guest list/main.js
+++ 16.New guest list/main.js
@@ -1,7 +1,7 @@
 var guest_list = ["sahiba", "kaneez", "noreen", "nousheen", "nazia"];
 var not_present = "nazia";
 var new_guest = "shama anjum";
-guest_list[1] = new_guest;
+guest_list[4] = new_guest;
 for (var i = 0; i < guest_list.length; i++) {
     console.log("Respected mam " +
         guest_list[i] +
